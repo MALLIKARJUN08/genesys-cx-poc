caf3b8882617e715561a3120464aa846390014db
MALLIKARJUN08 <innocentmallik8@gmail.com>
2026-03-16 12:40:01 +0530

    adding new support quueue

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "genesyscloud_routing_queue" "automated_queue" {
   name        = "Terraform_Automated_Queue"
   description = "This queue was created automatically via git push!"
 }
+
+resource "genesyscloud_routing_queue" "support_queue" {
+  name        = "Terraform_Support_Queue"
+  description = "A new support queue created via GitHub Actions"
+}
