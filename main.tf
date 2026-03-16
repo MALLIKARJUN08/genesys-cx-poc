61914f29513f6039471dede06a840fcce3fe6bb7
MALLIKARJUN08 <innocentmallik8@gmail.com>
2026-03-16 22:25:50 +0530

    Add moved blocks to handle project restructuring

--- main.tf
+++ main.tf
@@ -5,3 +5,23 @@ module "queues" {
 module "users" {
   source = "./modules/users"
 }
+
+moved {
+  from = genesyscloud_routing_queue.poc_queue
+  to   = module.queues.genesyscloud_routing_queue.poc_queue
+}
+
+moved {
+  from = genesyscloud_routing_queue.automated_queue
+  to   = module.queues.genesyscloud_routing_queue.automated_queue
+}
+
+moved {
+  from = genesyscloud_routing_queue.support_queue
+  to   = module.queues.genesyscloud_routing_queue.support_queue
+}
+
+moved {
+  from = genesyscloud_routing_queue.technical_queue
+  to   = module.queues.genesyscloud_routing_queue.technical_queue
+}
